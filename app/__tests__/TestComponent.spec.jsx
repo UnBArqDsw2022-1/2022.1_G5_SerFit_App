016afb27ce41920fb8242b322a0ba622c87a2a5e
SwampTG <adriansoareslps@gmail.com>
2022-07-10 16:15:35 -0300

    #1 - Fix tests e pasta viwes

--- app/__tests__/TestComponent.spec.jsx
+++ app/__tests__/TestComponent.spec.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import {
-	render
-} from '@testing-library/react-native';
+import { cleanup, render } from '@testing-library/react-native';
 import TestComponent from '../components/TestComponent';
+import { jest } from '@jest/globals';
 
-test('renders correctly', (done) => {
-  const comp = render(<TestComponent />);
-  const textComp = comp.getByText('Testing Component')
-  expect(textComp).not.toBeUndefined();
-  expect(textComp.children[0].type).toMatch('Text');
-  done();
+describe('App construction', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(cleanup);
+
+  it('renders correctly', (done) => {
+    const comp = render(<TestComponent />);
+    const textComp = comp.getByText('Testing Component');
+    expect(textComp).not.toBeUndefined();
+    expect(textComp.children[0].type).toMatch('Text');
+    done();
+  })
 });
