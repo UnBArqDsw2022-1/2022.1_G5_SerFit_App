e1440f543b929ad39002c6a0d79c992e7f101c16
SwampTG <adriansoareslps@gmail.com>
2022-09-04 23:39:56 -0300

    #12 - Tela de busca de profissionais
    Co-authored-by: Gustave <gpersijn@users.noreply.github.com>

---
+++ app/views/SearchProfessionals/index.jsx
@@ -0,0 +1,77 @@
+import { Keyboard, ScrollView, View } from "react-native";
+import SearchList from "./../../components/SearchList";
+import AppSearchBar from "./../../components/AppSearchBar";
+import React, { useEffect, useState } from "react";
+import loggedApi from "../../services/login";
+
+const SearchProfessionals = (props) => {
+  let [personals, setPersonals] = useState([]);
+  let [searchQuery, setSearchQuery] = useState("");
+  let [allPersonals, setAllPersonals] = useState([]);
+
+  const interestsTypes = [
+    "academia",
+    "danca",
+    "esportes radicais",
+    "ginastica",
+    "musculacao",
+    "natacao",
+    "outros",
+    "skate",
+    "surf",
+    "outros",
+  ];
+
+  const getPersonals = async () => {
+    return loggedApi
+      .get("/user/find", { params: { accountType: "personal" } })
+      .then((resp) => {
+        console.log(resp);
+        return resp.data;
+      })
+      .catch((error) => {
+        console.error("Error getting personals data: ", error);
+      });
+  };
+
+  useEffect(() => {
+    getPersonals().then((personals) => {
+      setPersonals(personals);
+      setAllPersonals(personals);
+    });
+  }, []);
+
+  return (
+    <View onPress={Keyboard.dismiss} onTouchMove={Keyboard.dismiss}>
+      <AppSearchBar
+        value={searchQuery}
+        placeholder="Pesquisa por profissional"
+        onChangeText={(input) => {
+          setSearchQuery(input);
+          if (!input) setPersonals(allPersonals);
+          else {
+            console.log(searchQuery);
+            console.log(personals);
+            if (interestsTypes.filter((item) => item === searchQuery))
+              setPersonals(
+                allPersonals.filter(
+                  (personal) => personal.mainInterest === input
+                )
+              );
+            else
+              setPersonals(
+                allPersonals.filter((item) => item.name?.includes(input))
+              );
+          }
+        }}
+        style={{}}
+        inputStyle={{}}
+      />
+      <ScrollView onPress={Keyboard.dismiss} onTouchMove={Keyboard.dismiss}>
+        <SearchList data={personals} />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default SearchProfessionals;
