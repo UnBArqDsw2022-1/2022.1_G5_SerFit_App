96a00b0f8e25dd8b8fb2dc6ee16c2f6f84d0f460
SwampTG <adriansoareslps@gmail.com>
2022-09-04 23:34:36 -0300

    #12 - Api com Header de acesso
    Co-authored-by: Gustave <gpersijn@users.noreply.github.com>

---
+++ app/services/login.js
@@ -0,0 +1,22 @@
+import api from './api';
+import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const getApi = async () => {
+    try {
+         let apiKey = await AsyncStorage.getItem('apiKey');
+        console.log('Testing async storage: ', apiKey);
+        api.defaults.headers.common['x-access-token'] = apiKey;
+        return apiKey;
+
+    } catch (error) {
+        console.log('Error getting apiKey', error);
+        return null;
+    }
+}
+
+getApi();
+console.log('The access token:', api.defaults.headers);
+
+let loggedApi = api;
+export default loggedApi;
